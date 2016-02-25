25a8f2a606dfbb1c4960f2ef1a14e64667e691d7
Dan Goodliffe <dan@randomdan.homeip.net>
2016-02-25 22:24:53 +0000

    Reuse prepared statements

--- libpqpp/pq-modifycommand.h
+++ libpqpp/pq-modifycommand.h
@@ -3,9 +3,9 @@
 
 #include <modifycommand.h>
 #include "pq-command.h"
+#include "pq-connection.h"
 
 namespace PQ {
-	class Connection;
 	class ModifyCommand : public DB::ModifyCommand, public Command {
 		public:
 			ModifyCommand(Connection *, const std::string & sql, unsigned int no);
@@ -14,8 +14,8 @@ namespace PQ {
 			unsigned int execute(bool) override;
 
 		private:
-			void prepare() const;
-			mutable bool prepared;
+			Connection::PreparedStatements::const_iterator prepare() const;
+			const Connection::StatementHash hash;
 	};
 }
 
