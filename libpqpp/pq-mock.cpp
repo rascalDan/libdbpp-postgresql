bfa6fe41faae214c0ad2287418914197862628e8
Dan Goodliffe <dan@randomdan.homeip.net>
2017-01-01 15:51:09 +0000

    Register mock db implementations

--- libpqpp/pq-mock.cpp
+++ libpqpp/pq-mock.cpp
@@ -5,6 +5,8 @@
 #include <selectcommand.h>
 #include <selectcommandUtil.impl.h>
 
+NAMEDFACTORY("postgresql", PQ::Mock, DB::MockDatabaseFactory);
+
 namespace PQ {
 
 Mock::Mock(const std::string & masterdb, const std::string & name, const std::vector<boost::filesystem::path> & ss) :
